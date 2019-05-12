2b91724aada2861ce01aa9a78da35c9df10d6fc9
Chris Goldsmith <48174822+ChrisGME4@users.noreply.github.com>
2019-05-12 22:37:56 +0100

    Add files via upload

---
+++ CSVImport.ps1
@@ -0,0 +1,53 @@
+﻿################################################
+# Configure variables below for connecting to the SQL database
+################################################
+$CSVFileName = "C:\Users\Datas.csv"
+$SQLInstance = "####"
+$SQLDatabase = "####"
+$SQLTable = "####"
+##############################################
+# Prompting for SQL credentials
+##############################################
+$SQLCredentials = Get-Credential -Message "Enter your SQL username & password"
+$SQLUsername = $SQLCredentials.UserName
+$SQLPassword = $SQLCredentials.GetNetworkCredential().Password
+##############################################
+# Start of time taken benchmark
+##############################################
+$Start = Get-Date
+##############################################
+# Importing CSV and processing data
+##############################################
+$CSVImport = Import-CSV $CSVFileName
+$CSVRowCount = $CSVImport.Count
+##############################################
+# ForEach CSV Line Inserting a row into the Temp SQL table
+##############################################
+"Inserting $CSVRowCount rows from CSV into SQL Table $SQLTable"
+ForEach ($CSVLine in $CSVImport)
+{
+# Setting variables for the CSV line
+$CSVProductId = $CSVLine.ProductId
+$CSVSpecificationId = $CSVLine.SpecificationId
+$CSVValue = $CSVLine.Value
+##############################################
+# SQL INSERT of CSV Line/Row
+##############################################
+$SQLInsert = "USE $SQLDatabase
+INSERT INTO $SQLTable (ProductId, SpecificationId, Value)
+VALUES('$CSVProductId', '$CSVSpecificationId', '$CSVValue m²');"
+# Running the INSERT Query
+Invoke-SQLCmd -Query $SQLInsert -ServerInstance $SQLInstance -Username $SQLUsername -Password $SQLPassword
+# End of ForEach CSV line below
+}
+# End of ForEach CSV line above
+##############################################
+# End of time taken benchmark
+##############################################
+$End = Get-Date
+$TimeTaken = New-Timespan -Start $Start -End $End | Select -ExpandProperty TotalSeconds
+$TimeTaken = [Math]::Round($TimeTaken, 0)
+"CSV Import Finished In $TimeTaken Seconds"
+##############################################
+# End of script
+##############################################
